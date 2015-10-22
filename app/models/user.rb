b85298a1b879be9c644d73585df7a98182f07e48
Claire Thompson <cthompson@pivotal.io>
2015-10-22 14:15:33 -0700

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  before_save { self.phone_number = self.phone_number.tr('^0-9', '')}
+
+  VALID_PHONE_NUMBER_REGEX = /\A(\+0?1\s)?\(?\d{3}\)? ?-?[\s.-]?\d{3}[\s.|-]?\d{4}\z/
+  validates :phone_number, presence: true,
+                           format: {with: VALID_PHONE_NUMBER_REGEX },
+                           uniqueness: true
+  before_validation do
+    self.phone_number = self.phone_number.tr('^0-9', '')
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :each do
+    @user = User.new(phone_number: '4564564567')
+  end
+
+  it 'should be valid' do
+    expect(@user).to be_valid
+  end
+
+  it 'should have a phone number' do
+    @user.phone_number = ''
+    expect(@user).not_to be_valid
+  end
+
+  describe 'the phone number' do
+    it 'should be only digits' do
+      @user.phone_number = 'i am not a number'
+      expect(@user).not_to be_valid
+      @user.phone_number = '1234 blah'
+      expect(@user).not_to be_valid
+      @user.phone_number = 'blh 1234'
+      expect(@user).not_to be_valid
+    end
+
+    it 'should accept different number formats' do
+      @user.phone_number = '456-456-4567'
+      expect(@user).to be_valid
+      @user.phone_number = '(456)456-4567'
+      expect(@user).to be_valid
+      @user.phone_number = '(456) 456-4567'
+      expect(@user).to be_valid
+    end
+
+    it 'should be unique from other numbers' do
+      same_number_user = User.new(phone_number: '456-456-4567')
+      @user.save
+      expect(same_number_user).not_to be_valid
+    end
+  end
+end
